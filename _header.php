f65c4fbfe15dbd8c6e472047086ac992c0218836
Juninzin Mlq zika <41973811+juze123@users.noreply.github.com>
2018-08-09 19:50:50 -0300

    Add files via upload

---
+++ _header.php
@@ -0,0 +1,45 @@
+﻿<?php
+if($_SERVER['SERVER_NAME'] == 'localhost') {
+   header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+   header('Cache-Control: no-store, no-cache, must-revalidate');
+   header('Cache-Control: post-check=0, pre-check=0', FALSE);
+   header('Pragma: no-cache');
+}
+?>
+<!DOCTYPE html>
+<head>
+	<title>Salve o Mundão</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="icon" href="favicon.png">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/global.css">
+</head>
+<body>
+
+
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="index.php#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">SOBRE</a>
+    <a href="index.php#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">SHOWS</a>
+    <a href="index.php#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTATOS</a>
+	<a href="listartigos.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">MÚSICAS</a>
+   
+   
+  </div>
+</div>
+
+
+<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+  <a href="index.php#band" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">SOBRE</a>
+  <a href="index.php#tour" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">SHOWS</a>
+  <a href="index.php#contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CONTATOS</a>
+  <a href="listartigos.php" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MÚSICAS</a>
+</div>
+	</nav>
+	<content>
---
+++ leartigo.php
@@ -0,0 +1,48 @@
+﻿<?php
+//header
+require ('_header.php');
+
+// Obtém ID do artigo solicitado
+if(isset($_GET['id'])) {
+	$id = intval($_GET['id']);
+} else {
+	header('Location: /listartigos.php');
+}
+
+// Conexão com base de dados
+include_once("conexao.php");
+
+// Query que lê o artigo, mas somente dentro da data e com status válido
+$sql = "SELECT *, DATE_FORMAT(data, '%d\/%m\/%Y \à\s %H:%i') AS databr FROM artigos WHERE id = '{$id}' AND data < NOW() AND status = '1'";
+
+// Executa a query
+$res = mysqli_query($conn, $sql);
+
+// Verifica se realmente achou 1 artigo
+if(mysqli_num_rows($res) !== 1) {
+	header('Location: /listartigos.php');
+}
+
+
+// Loop para "varrer" artigos
+$artigo = mysqli_fetch_assoc($res);
+
+// Lista de categorias em um array
+$cat = explode(",", $artigo['categorias']);
+
+
+echo "<br>&nbsp;<h2>{$artigo['titulo']}</h2>\n";
+echo "<p><i>Por {$artigo['autor']} em {$artigo['databr']}.</i></p>\n";
+echo "<div class=\"textoArtigo\">{$artigo['texto']}</div>\n";
+echo "<h4>Categorias</h4>\n";
+echo "<ul>\n";
+foreach($cat AS $listCat) {
+	$listCat = trim($listCat);
+	echo "<li>{$listCat}</li>\n";
+}
+echo "</ul>\n";
+echo "<p><a href=\"\listartigos.php\">Lista de Artigos</a></p>\n";
+// footer
+require ('_footer.php');
+
+?>
